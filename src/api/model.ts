58ce4c1a98879abc87afc56191079c0fe5987368
Oriza Sativa Cikal.M <oryzasativacikal@gmail.com>
2025-08-20 15:55:28 +0700

    finish all

--- src/api/model.ts
+++ src/api/model.ts
@@ -2,7 +2,7 @@ import axios from "axios";
 import { axiosInterceptor } from "./axiosInterceptor";
 
 export const uploadModel = async (form: any, callback: any) => {
-    await axios.post(' http://127.0.0.1:5000/predict', form)
+    await axios.post(' https://daraprasidang2.pythonanywhere.com/predict', form)
         .then((result) => {
             callback(result.data)
         }).catch((err) => {
